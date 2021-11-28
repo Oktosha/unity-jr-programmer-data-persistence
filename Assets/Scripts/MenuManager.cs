1acc7ed3b6dc4ac1ea56f7f74260ae640c12082e
Daria Kolodzey <kolodaria@gmail.com>
2021-11-28 19:07:01 +0100

    Make start and quit buttons work in menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+}
